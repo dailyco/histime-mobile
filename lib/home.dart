86e79b51da4aa168a71c2d297724f99c5fd0594d
YuJin-Kim <21700140@handong.edu>
2019-11-18 21:38:40 +0900

    building home page

--- lib/home.dart
+++ lib/home.dart
@@ -15,16 +15,135 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _mybody(),
+      bottomNavigationBar: _makeBottom(),
+    );
+  }
+
+  Widget _makeBottom() {
+    return Container(
+      height: 55.0,
+      child: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.black),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.storage, color: Colors.black),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   Widget _mybody() {
+    final _logo = Column(
+      children: <Widget>[
+        Image.asset(
+          'image/logo_small.png',
+          width: 80,
+          height: 20,
+          fit: BoxFit.contain,
+        ),
+        Text(
+          '이번 학기를 채울 모든 경우의 수',
+          style: TextStyle(fontSize: 7),
+        ),
+      ],
+    );
+
+    final _top = Row(
+      children: <Widget>[
+        _logo,
+        Expanded(
+          child: SizedBox(),
+        ),
+        Text(
+          "님, 반가워요!",
+          style: TextStyle(fontSize: 12),
+        ),
+        SizedBox(
+          width: 7,
+        ),
+        SizedBox(
+          width: 80,
+          height: 25,
+          child: RaisedButton(
+            color: const Color(0xFFFFCA55),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(55)),
+            child: Text('Logout'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
+    );
+
+    final makeListTile = ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Colors.white24))),
+          child: Icon(Icons.autorenew, color: Colors.white),
+        ),
+        title: Text(
+          "Introduction to Driving",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
+
+        subtitle: Row(
+          children: <Widget>[
+            Icon(Icons.linear_scale, color: Colors.yellowAccent),
+            Text(" Intermediate", style: TextStyle(color: Colors.white))
+          ],
+        ),
+        trailing:
+        Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0));
+
+    final makeCard = Card(
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+        child: makeListTile,
+      ),
+    );
+
+    final _body = Container(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard;
+        },
+      ),
+    );
+
     return SafeArea(
-      child: Text("Hello"),
+      child: Container(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            _top,
+            _body,
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/login.dart
+++ lib/login.dart
@@ -75,7 +75,7 @@ class _LoginPageState extends State<LoginPage> {
               Navigator.push(context, MaterialPageRoute(builder: (context)
                     => HomePage()));
             },
-          )
+          ),
         ],
       ),
     ) ;
